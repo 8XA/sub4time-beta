282a4f4234611de3bfe3db1caeaa57055946ece9
8XA <69772360+8XA@users.noreply.github.com>
2021-01-18 15:02:20 -0700

    Instalador ya agrega alias

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ pkg install -y wget
 pkg install -y iconv
 pkg install -y unrar
 echo "clear" >> ../usr/etc/bash.bashrc
-echo "alias sub='python /data/data/com.termux/files/usr/share/sub4time/sub4time/sub.py' >> ../usr/etc/bash.bashrc"
+echo "alias sub='python /data/data/com.termux/files/usr/share/sub4time/sub4time/sub.py'" >> ../usr/etc/bash.bashrc
 clear
 echo "Instalación completa. Reinicia Termux."
 kill -9 $PPID

0e2ca166bee47a9effa7bc2df64fdefbf35766cd
8XA <ochoa.ssa@gmail.com>
2021-01-21 01:29:49 -0700

    Hotfix para el instalador

--- installer.sh
+++ installer.sh
@@ -3,7 +3,7 @@ pkg install -y python
 pkg install -y wget
 pkg install -y iconv
 pkg install -y unrar
-pip install -y termcolor
+pip install termcolor
 echo "clear" >> ../usr/etc/bash.bashrc
 echo "alias sub='python /data/data/com.termux/files/usr/share/sub4time/sub4time/sub.py'" >> ../usr/etc/bash.bashrc
 clear

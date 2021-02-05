d068e7c1bfd4a492fe64f437fe6659351d0522d0
8XA <ochoa.ssa@gmail.com>
2021-02-05 02:49:21 -0700

    Instalador no agrega borrado de pantalla al inicio de termux

--- installer.sh
+++ installer.sh
@@ -5,7 +5,6 @@ pkg install -y iconv
 pkg install -y unrar
 pkg install -y readline
 pip install termcolor
-echo "clear" >> ../usr/etc/bash.bashrc
 echo "alias sub='exec python /data/data/com.termux/files/usr/share/sub4time/sub4time/sub.py'" >> ../usr/etc/bash.bashrc
 clear
 termux-setup-storage
